4f2a0d81222b2663cdbae7c171e3c4a9028c8f19
Fran Jurinec <fran.jurinec@yubico.com>
2024-03-30 17:15:55 +0200

    Fixup Egg / Transition

--- Assets/Scripts/LockInput.cs
+++ Assets/Scripts/LockInput.cs
@@ -1,36 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 public class LockInput : MonoBehaviour
 {
+    public SceneTransition sceneTransition;
+    public EggSceneController eggSceneController;
     public TMP_InputField inputField1;
     public TMP_InputField inputField2;
     public TMP_InputField inputField3;
     //public Text errorMessageText;
-
-    private bool egg = false; // Variable to store the state of egg
 
     public void OnSubmitButtonPressed()
     {
         // Check if the input values match the desired values
         if (inputField1.text == "4" && inputField2.text == "2" && inputField3.text == "0")
         {
-            egg = true; // Set egg variable to true if the code is correct
             Debug.Log("Code is correct, u have an egg!");
+            sceneTransition.egg = true;
+            eggSceneController.ClosePanel();
         }
         else
         {
-            egg = false; // Set egg variable to false if the code is incorrect
             Debug.Log("Code is incorrect!");
             //errorMessageText.text = "Code incorrect"; // Display error message
         }
-    }
-
-    public bool GetEgg()
-    {
-        return egg;
     }
 }
--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -1,53 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
     public Transform transitionPoint; // Specific point in the next scene where the player will appear
-
-    private LockInput lockInput;
-
-    private void Start()
-    {
-        // Find and store the LockInput script reference
-        lockInput = FindObjectOfType<LockInput>();
-        if (lockInput == null)
-        {
-            Debug.LogError("LockInput script not found in the scene.");
-        }
-    }
+    public bool egg = false;
 
     private void OnTriggerEnter2D(Collider2D other)
 
     {
-        Debug.Log("LockInput reference: " + lockInput); // Check if lockInput is not null
-        if (lockInput != null)
+        Debug.Log("Egg value: " + egg); // Check the value returned by GetEgg()
+        if (other.CompareTag("Player"))
         {
-            Debug.Log("Egg value: " + lockInput.GetEgg()); // Check the value returned by GetEgg()
-        }
-        else
-        {
-            Debug.LogError("LockInput reference is null.");
-        }
-        //if (other.CompareTag("Player"))
-        //{
-        //    // Check if the player is allowed to transition to the next scene based on the egg value
-        //    if (lockInput != null && lockInput.GetEgg())
-        //    {
-        //        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            // Check if the player is allowed to transition to the next scene based on the egg value
+            if (egg)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
-        //        if (transitionPoint != null)
-        //        {
-        //            other.transform.position = transitionPoint.position;
-        //        }
-        //    }
-        //    else
-        //    {
-        //        Debug.Log("Egg is false. Player cannot transition to the next scene.");
-        //    }
-        //}
+                if (transitionPoint != null)
+                {
+                    other.transform.position = transitionPoint.position;
+                }
+            }
+            else
+            {
+                Debug.Log("Egg is false. Player cannot transition to the next scene.");
+            }
+        }
     }
 }
 
