551308438a1890b50c7c2d6a22c0e72f0eb13c04
Lucija Filipovic <lucija_filipovic_@hotmail.com>
2024-03-30 03:38:05 +0200

    Adding egg scene graphics, walking frog, and pause menu

--- Assets/Scripts/StartingScreenScript.cs
+++ Assets/Scripts/StartingScreenScript.cs
@@ -5,6 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class StartingScreenScript : MonoBehaviour
 {
+    public GameObject menuPanel;
+
+    // Cache the screen bounds to avoid recalculating them every update
+    private float screenWidth;
+    private float screenHeight;
+
+    private void Start()
+    {
+        menuPanel.SetActive(false);
+        Time.timeScale = 1f;
+
+        // Calculate and cache the screen bounds
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (IsGamePaused())
+            {
+                ResumeGame();
+                print("Resuming game: " + Time.timeScale);
+            }
+            else
+            {
+                PauseGame();
+                print("Pausing game: " + Time.timeScale);
+            }
+        }
+    }
+
     public void PlayGame()
     {
         //Load next scene in the build level
@@ -18,4 +51,24 @@ public class StartingScreenScript : MonoBehaviour
         //Quit the application
         Application.Quit();
     }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        menuPanel.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        menuPanel.SetActive(false);
+    }
+
+    // Function to check if the game is currently paused
+    private bool IsGamePaused()
+    {
+        return Time.timeScale == 0f;
+    }
+
+
 }
