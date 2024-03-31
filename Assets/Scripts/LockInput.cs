7fccab115131767451e59949c92b832e54d7ca17
Fran Jurinec <fran.jurinec@yubico.com>
2024-03-31 02:19:47 +0200

    Blueberry Pickup and Transition

--- Assets/Scripts/LockInput.cs
+++ Assets/Scripts/LockInput.cs
@@ -18,10 +18,10 @@ public class LockInput : MonoBehaviour
         // Check if the input values match the desired values
         if (inputField1.text == "4" && inputField2.text == "2" && inputField3.text == "0")
         {
-            
+
             Debug.Log("Code is correct, u have an egg!");
             errorMessage.text = "Code correct! You got an egg.";
-            sceneTransition.egg = true;
+            sceneTransition.itemCollected = true;
             eggSceneController.ClosePanel();
             eggPanel.StartDialog();
         }
