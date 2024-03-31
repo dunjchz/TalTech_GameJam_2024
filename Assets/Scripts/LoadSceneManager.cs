edb50a3387c88bb497b6ba1d332e110fb69f3ded
Lucija Filipovic <lucija_filipovic_@hotmail.com>
2024-03-31 05:27:10 +0300

    Cutscene Intro

---
+++ Assets/Scripts/LoadSceneManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneManager : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+}
